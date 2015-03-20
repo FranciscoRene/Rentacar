da4b7020df2d4504a1fed9c51668de9d6306cdcd
franciscoRene <franciscorenef@gmail.com>
2015-03-20 16:59:35 -0400

    edición del proyecto

--- rentacar/db.inc.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-$dbhost = "localhost" ;
-$dbuser = "rentacar";
-$dbpass = "rentacar" ;
-$db = "rent_a_car" ;
-$conectar = mysql_connect($dbhost,$dbuser,$dbpass) ; mysql_select_db($db,$conectar) ;
-?>
